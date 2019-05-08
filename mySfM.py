6ef762f595b89bdb02284a1a53a2d4aa094aefd2
sayeayed <sayeayed@gmail.com>
2019-05-08 16:07:06 -0400

    Created empty file for package definitions

---
+++ mySfM.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 15:51:59 2019
+
+@author: Ayeda Sayeed
+
+Package to implement Structure from Motion techniques to reconstruct a 
+3D object from a series of 2D images
+
+*not yet complete*
+
+"""
+
+import numpy as np
+import cv2
+
+class MatchedPoint():
+    def _init_(self):
+        ...
+        
+
+def triangulateFun():
+    ...
+
+def reconstructFun():
+    ...
