ea9f4748d6cd6c89a48e5b6fdedc141bfae6bed0
sayeayed <sayeayed@gmail.com>
2019-05-08 21:36:50 -0400

    style: comment structure modifications

--- mySfM.py
+++ mySfM.py
@@ -13,14 +13,24 @@ Package to implement Structure from Motion techniques to reconstruct a
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class MatchedPoint():
+    '''Assuming we have the point correspondences, we input them
+    into this object for easy manipulation'''
     def _init_(self):
         '''...'''
-        
 
 def triangulateFun():
-    '''...'''
+    '''Find the 3D points in global coordinates using a pair of camera
+    poses and point correspondences'''
 
-def reconstructFun():
-    '''...'''
+def reconstructScatterFun():
+    '''Generate a simple 3D scatter plot'''
+
+def reconstructPLYFun():
+    '''Generate a PLY file, using plyfile, to be opened in MeshLab or Blender'''
+    
+def reconstructPCDFun():
+    '''Generate a coloured point cloud PCD, using pypcd'''
